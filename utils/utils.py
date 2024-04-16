4bcd9dcc5be572aadecf923bec98794600bbbc35
AniTho <aniketthomas27@gmail.com>
2024-04-16 23:41:01 +0530

    Added diffusion logger to utils

--- utils/utils.py
+++ utils/utils.py
@@ -5,6 +5,8 @@ from torch.utils.data import DataLoader
 import torch
 from models.generative_model import build_autoencoder
 import os
+from tqdm import tqdm
+import numpy as np
 
 def get_transforms(cfg):
     return transforms.Compose([
@@ -48,3 +50,27 @@ def ldm_scaling_factor(cfg, train_dataset):
         z = autoencoder.encode_stage_2_inputs(eda_data.to(device))
 
     return 1 / torch.std(z)
+
+@torch.no_grad()
+def log_ldm_sample_unconditioned(diffusion_model, auto_sampler, text_encoder,
+    scheduler, spatial_shape, device, scale_factor, epoch):
+    latent = torch.randn((1,) + spatial_shape)
+    latent = latent.to(device)
+
+    prompt_embeds = torch.cat((49406 * torch.ones(1, 1), 49407 * torch.ones(1, 76)), 1).long()
+    prompt_embeds = prompt_embeds.to(device)
+    prompt_embeds = text_encoder(prompt_embeds.squeeze(1))
+    prompt_embeds = prompt_embeds[0]
+
+    for t in tqdm(scheduler.timesteps, ncols=70, leave=False):
+        noise_pred = diffusion_model(x=latent, 
+                                    timesteps=torch.asarray((t,)).to(device), 
+                                    context=prompt_embeds)
+        latent, _ = scheduler.step(noise_pred, t, latent)
+
+    x_hat = auto_sampler.model.decode(latent / scale_factor)
+    img_0 = np.clip(a=x_hat[0, 0, :, :].cpu().numpy(), a_min=0, a_max=1)
+    fig = plt.figure(dpi=300)
+    plt.imshow(img_0, cmap="gray")
+    plt.axis("off")
+    plt.savefig(f"images/ldm_sample_gen/unconditioned_{epoch}.png")
